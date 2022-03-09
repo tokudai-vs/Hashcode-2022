077bf677effdb82089589f0fc4863bdf5d3cfe97
Vishal Singh <vishalvsy1996@gmail.com>
2022-03-09 12:35:09 +0530

    Update Qualification.py
    Changes by @kgautam01

--- Qualification.py
+++ Qualification.py
@@ -29,27 +29,29 @@ projects = sorted(projects,key=lambda x: x[3])
 count = 0
 time = 0
 res = []
-
 for project in projects:
-
     roles = project[5]
-    people = []
+    people = {}
     for r in roles:
         req_role, req_level = r[0], r[1]
         for contri in contributers:
             name, skill = contri[0], contri[1]
             for s in skill:
                 if  req_role==s[0] and s[1] >= req_level:
-                    if not name in set(people):
-                        people.append(name)
+                    if not name in set(list(people.values())):
+                        people[req_role]=name
                         break
     if len(people) == len(roles):
         count += 1
         time += project[1]
         res.append([project[0], people])
 
+    
+    # print(people)
 
+# print(res)
 print(count)
 for val in res:
     print(val[0])
-    print(" ".join(val[1]))
+    # print(" ".join(val[1]))
+    print(*list(val[1].values()))
